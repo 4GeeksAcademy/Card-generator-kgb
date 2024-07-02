593d512d3529765fad77e35aeb067b7ad0668be8
KGBeneby <167141463+KGBeneby@users.noreply.github.com>
2024-07-02 15:18:14 +0000

    card  generator complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♥", "♠", "♣", "♦"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const topSuitElement = document.getElementById("top-suit");
+  const valueElement = document.getElementById("value");
+  const bottomSuitElement = document.getElementById("bottom-suit");
+
+  topSuitElement.textContent = randomSuit;
+  valueElement.textContent = randomValue;
+  bottomSuitElement.textContent = randomSuit;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    topSuitElement.style.color = "red";
+    valueElement.style.color = "red";
+    bottomSuitElement.style.color = "red";
+  }
 };
